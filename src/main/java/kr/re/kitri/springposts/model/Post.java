ab192811c3088fe50f5d8b5670a23970879f37be
7ingout <hadvjin@naver.com>
2024-07-18 14:51:02 +0900

    예외처리

--- src/main/java/kr/re/kitri/springposts/model/Post.java
+++ src/main/java/kr/re/kitri/springposts/model/Post.java
@@ -1,6 +1,5 @@
 package kr.re.kitri.springposts.model;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +9,17 @@ import org.springframework.data.relational.core.mapping.Table;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name ="post_hj")
+@Table(name = "post")
 public class Post {
     @Id
     private long id;
     private String title;
     private String body;
     private int likes;
+
+    public Post(String title, String body, int likes) {
+        this.title = title;
+        this.body = body;
+        this.likes = likes;
+    }
 }
